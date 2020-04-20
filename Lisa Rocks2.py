ddb279e016026e34a32a3644ad15be695bd4d6d3
LisaManisa <64044075+LisaManisa@users.noreply.github.com>
2020-04-20 17:29:40 -0400

    Add files via upload

---
+++ Lisa Rocks2.py
@@ -0,0 +1,21 @@
+# Day 1
+
+# Challenge 1: Make a program that prints a message to the screen.
+# Challenge 2: Ask the user what they want to print, and put their answer in a variable. Print whatever is in the variable.
+# Challenge 3: After asking what to print, ask how many times to print it. Store this in a variable, and don't do anything with it for now.
+# Challenge 4: Check if they asked to print it 0 times. If they did, print a message to the screen to tell them.
+# Challenge 5: Check if the thing they typed is a number. You may need to google how to do this. If it isn't, tell them.
+# Challenge 6: In both steps 4 and 5, instead of printing a message, create an error. You might need to google how to do this.
+
+# Do as many of these challenges as you can.
+
+print ("Hello Everybody!!")
+print ("What is your name?")
+myName = input()
+print ("Thanks for playing with my Python program, " + myName)
+print ("What do you want to print?")
+list = input ()
+print ("That's good that you want to print " + list)
+
+
+
