af4d963bc5b2eca4ed368147f0a89cad8171b188
djShmooShmoo <44006703+djShmooShmoo@users.noreply.github.com>
2020-04-20 17:24:15 -0400

    Create Lesson3.py

---
+++ Lesson3.py
@@ -0,0 +1,19 @@
+print("Hello World!")
+# if you have a line of text that you want to remove,
+#"comment it out" by adding in a hashtag.
+# print("Hello World!")
+# text in Python is always in quotation marks
+print("Lisa")
+print("Hello World. Lisa is cool")
+print("Lisa said, 'I love you'")
+print('Lisa said, "I love you"')
+# if you put anything in single quotes, it won't run.
+print (print("Hello World!"))
+print("Hello World!")
+print("I'm the smartest in the class!!")
+
+# use backslash for ignoring the next thing in the code.
+print('print("That\'s Mine!")')
+
+# \n means new line
+print("Hello\nWorld")
